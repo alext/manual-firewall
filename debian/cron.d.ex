89f871146cdfaa6aece813aa8fc8afa54b0886f4
Alex Tomlins <alex.tomlins@unboxedconsulting.com>
2011-01-24 15:35:27 +0000

    Removed sample files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the manual-firewall package
-#
-0 4	* * *	root	[ -x /usr/bin/manual-firewall_maintenance ] && /usr/bin/manual-firewall_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(manual-firewall):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="manual-firewall" command="/usr/bin/manual-firewall"
